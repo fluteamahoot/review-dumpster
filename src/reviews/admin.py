4604f16b719e5b6733a6addf779625bceacea0c5
Randy Butts <rbutts@chq.org>
2018-03-06 18:47:11 -0500

    tweaked gitignore and registered review models in admin

--- src/reviews/admin.py
+++ src/reviews/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Review)
+class ReviewAdmin(admin.ModelAdmin):
+    fields = ['media', 'title', 'body', 'verdict']
+
+    def save_model(self, request, obj, form, change):
+        if getattr(obj, 'author', None) is None:
+            obj.author = request.user
+        obj.save()
+
+    list_display = ['author', 'media', 'title', 'verdict', 'created', 'last_modified']
+
+
+admin.site.register(models.Media)
+admin.site.register(models.Publisher)
